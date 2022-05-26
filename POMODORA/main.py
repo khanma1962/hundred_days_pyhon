39acac202a9fb60086551f673e951fba688fd84a
Moe Khan <m_a_khan_usa@yahoo.com>
2022-05-25 22:43:01 -0700

    Everything is working.

--- POMODORA/main.py
+++ POMODORA/main.py
@@ -1,5 +1,4 @@
 from tkinter import *
-import time
 import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -9,15 +8,25 @@ GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 BACKGROUND = "#FFE3A9"
 FONT_NAME = "Courier"
-WORK_MIN = 1  # 25
-SHORT_BREAK_MIN = 1  # 5
-LONG_BREAK_MIN = 2  # 20
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
-    pass
+    window.after_cancel(timer)
+    #go back to 00:00
+    canvas.itemconfig(timer_text, text = "00:00")
+    #title changes to 'Timer'
+    title_label.config(text = "Timer")
+    #reset check_marks
+    check_marks.config(text = " ")
+    global reps
+    reps = 0
+
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
@@ -53,9 +62,15 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{min}:{sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        marks = ""
+        work_session = math.floor(reps / 2)
+        for _ in range(work_session):
+            marks += "✅ "
+        check_marks.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -66,7 +81,8 @@ window.title("Pomodora")
 window.config(padx=100, pady=50, bg=BACKGROUND)
 
 # title_label = Label(text='Timer', fg=GREEN, bg=BACKGROUND,  font= (FONT_NAME, 45, 'bold'))
-# title_label.grid(column=1, row=0)
+title_label = Label(text= "Timer", fg=PINK, bg=BACKGROUND, font=(FONT_NAME, 45, 'bold'))
+title_label.grid(column=1, row=0)
 
 canvas = Canvas(width=200, height=224, bg=BACKGROUND, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
@@ -81,7 +97,7 @@ start_button.grid(column=0, row=2)
 start_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 start_button.grid(column=2, row=2)
 
-check_marks = Label(text="✅")  # ✔") #✅
+check_marks = Label()  # ✔") #✅
 check_marks.grid(column=1, row=2)
 
 # start_timer()
